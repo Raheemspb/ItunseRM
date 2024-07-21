436c9dacb6736aa201b4e1c994057e5cb2794d21
Raheemspb <gabibovda@gmail.com>
2024-07-21 17:42:43 +0300

    Create CollectionViewCell and RealmModel

---
+++ ItunseRM/ItunseRM/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  ItunseRM
+//
+//  Created by Рахим Габибли on 21.07.2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ItunseRM/ItunseRM/RMRealmClass.swift
@@ -0,0 +1,8 @@
+//
+//  RMRealmClass.swift
+//  ItunseRM
+//
+//  Created by Рахим Габибли on 21.07.2024.
+//
+
+import Foundation
